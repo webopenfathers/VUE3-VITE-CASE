6f5cdf702f8aa78f1ed0bb7bf1f64700d2686527
zwx1132021 <1290443925@qq.com>
2024-03-27 21:29:56 +0800

    2024 3-27 21:28 优化聊天支持-自定义图片宽高 && 发送文字 && 发送视频

--- src/components/3-userChatList.vue
+++ src/components/3-userChatList.vue
@@ -8,24 +8,49 @@
         :key="index"
         :style="{ 'flex-direction': item.flag ? 'row' : 'row-reverse' }"
       >
-        <!-- 头像 -->
         <img :src="item.avatar" alt="" />
-        <!-- 文字 或 图片 -->
+        <!-- 纯文字 -->
         <div v-if="item.type === 1" class="text-word">{{ item.text }}</div>
+        <!-- 纯图片 -->
         <div
           v-if="item.type === 2"
           class="text-img"
-          :style="{ height: getImageHeight(item.text) + 'px' }"
+          :style="{ width: item.imgW + 'px', height: item.imgH + 'px' }"
         >
           <el-image :src="item.text" :preview-src-list="[item.text]">
           </el-image>
         </div>
-        <!-- 文字 或 图片 -->
+        <!-- 纯视频 -->
+        <div v-if="item.type === 3" class="text-video">
+          <video
+            width="300px"
+            height="180px"
+            controls="true"
+            :poster="item.poster"
+          >
+            <source :src="item.text" type="video/mp4" />
+          </video>
+        </div>
       </div>
     </div>
 
     <!-- 底部键盘 -->
     <div class="send">
+      <el-icon
+        color="#409eff"
+        size="40"
+        style="margin-right: 5px"
+        @click="fileRef.click()"
+        ><Picture
+      /></el-icon>
+      <input
+        type="file"
+        ref="fileRef"
+        multiple
+        style="display: none"
+        @change="changeFile"
+        accept=".png,jpg"
+      />
       <el-input v-model="inputText"></el-input>
       <el-button
         class="send-button"
@@ -39,11 +64,25 @@
 </template>
 
 <script setup>
-import { ref, nextTick } from 'vue'
+import { ref, nextTick, onMounted } from 'vue'
+// 上传文件Ref
+const fileRef = ref(null)
 const inputText = ref('')
 // 此处userId 1在左  0在右
 const userId = ref(1)
 const list = ref([
+  {
+    avatar: new URL('../assets/touxiang.png', import.meta.url).href,
+    text: '99999999',
+    type: 1, // 1.文字 2.图片
+    flag: 1, // 1 在左 0 在右 真是开发依据用户ID
+  },
+  {
+    avatar: new URL('../assets/touxiang2.png', import.meta.url).href,
+    text: 'zbw_test',
+    type: 1, // 1.文字 2.图片
+    flag: 0, // 1 在左 0 在右 真是开发依据用户ID
+  },
   {
     avatar: new URL('../assets/touxiang.png', import.meta.url).href,
     text: '99999999',
@@ -64,41 +103,68 @@ const list = ref([
   },
   {
     avatar: new URL('../assets/touxiang2.png', import.meta.url).href,
+    text: 'https://img1.sycdn.imooc.com/szimg/65f2a35c081b354705400304.jpg',
+    type: 2, // 1.文字 2.图片
+    flag: 1, // 1 在左 0 在右 真是开发依据用户ID
+  },
+  {
+    avatar: new URL('../assets/touxiang2.png', import.meta.url).href,
     text: 'https://images.pexels.com/photos/5313576/pexels-photo-5313576.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
     type: 2, // 1.文字 2.图片
     flag: 1, // 1 在左 0 在右 真是开发依据用户ID
   },
+  {
+    avatar: new URL('../assets/touxiang2.png', import.meta.url).href,
+    text: 'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/xgplayer-demo-720p.mp4',
+    poster: new URL('../assets/load.jpg', import.meta.url).href,
+    type: 3, // 1.文字 2.图片
+    flag: 0, // 1 在左 0 在右 真是开发依据用户ID
+  },
 ])
 
-// 动态计算每张图片的高度并返回
+// 初始化所有图片宽高
+const initImage = () => {
+  list.value.forEach(async (item, index) => {
+    if (item.type === 2) {
+      const { imgW, imgH } = await getImageHeight(item.text)
+      item.imgW = imgW
+      item.imgH = imgH
+    }
+  })
+}
+
+// 同步获取图片宽高
 const getImageHeight = (url) => {
   let img = new Image()
   let aspectRatio = 0
   img.src = url
-  img.onload = () => {
-    aspectRatio = img.width / img.height
-    return 200 / aspectRatio
-  }
+  return new Promise((resolve) => {
+    img.onload = () => {
+      aspectRatio = img.width / img.height
+      let wFlag = 250 > img.width
+      if (wFlag) {
+        resolve({
+          imgW: img.width,
+          imgH: img.height,
+        })
+      } else {
+        resolve({
+          imgW: 250,
+          imgH: 250 / aspectRatio,
+        })
+      }
+    }
+  })
 }
 
-//发送按钮
+// 发送文字按钮
 const sendMsg = () => {
-  // 测试发送不同的数据
-  if (userId.value === 1) {
-    list.value.push({
-      avatar: new URL('../assets/touxiang.png', import.meta.url).href,
-      text: inputText.value,
-      type: 1, // 文字
-      flag: userId.value, // 1 在左 0 在右 真是开发依据用户ID
-    })
-  } else {
-    list.value.push({
-      avatar: new URL('../assets/touxiang2.png', import.meta.url).href,
-      text: 'https://images.pexels.com/photos/10413401/pexels-photo-10413401.jpeg',
-      type: 2, // 图片
-      flag: userId.value, // 1 在左 0 在右 真是开发依据用户ID
-    })
-  }
+  list.value.push({
+    avatar: new URL('../assets/touxiang.png', import.meta.url).href,
+    text: inputText.value,
+    type: 1, // 文字
+    flag: userId.value, // 1 在左 0 在右 真是开发依据用户ID
+  })
 
   inputText.value = ''
   if (userId.value === 0) {
@@ -106,13 +172,48 @@ const sendMsg = () => {
   } else {
     userId.value = 0
   }
+  goToBottom()
 }
+
+// 自动滚到底部
+const goToBottom = () => {
+  setTimeout(() => {
+    const scrollElem = document.querySelector('.content-box')
+    scrollElem.scrollTo({
+      top: scrollElem.scrollHeight,
+      behavior: 'smooth',
+    })
+  }, 200)
+}
+
+// 发送图片逻辑
+const changeFile = (e) => {
+  const fileList = Array.from(e.target.files)
+  fileList.forEach(async (item) => {
+    const url = window.URL.createObjectURL(item)
+    const { imgW, imgH } = await getImageHeight(url)
+    list.value.push({
+      avatar: new URL('../assets/touxiang2.png', import.meta.url).href,
+      text: url,
+      imgW,
+      imgH,
+      type: 2,
+      flag: 1,
+    })
+  })
+  goToBottom()
+}
+
+onMounted(() => {
+  initImage()
+  goToBottom()
+})
 </script>
 
 <style scoped>
 .main-box {
-  width: 600px;
-  height: 800px;
+  width: 900px;
+  height: 950px;
   margin: 10px auto;
   border: 1px solid #ccc;
   position: relative;
@@ -156,7 +257,13 @@ const sendMsg = () => {
 }
 
 .text-img {
-  width: 200px;
+  margin: 5px;
+  border-radius: 5px;
+  padding: 10px;
+  background-color: #fff;
+}
+
+.text-video {
   margin: 5px;
   border-radius: 5px;
   padding: 10px;
@@ -165,5 +272,6 @@ const sendMsg = () => {
 
 .send-button {
   margin-left: 10px !important;
+  height: 40px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 // 批量注册自定义全局指令
 import { setDirective } from './directives/index.js'
 
 const app = createApp(App)
 app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(setDirective)
 app.mount('#app')
