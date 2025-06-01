fc6351ce22ea901c743ed59b8f8ac134a58be5c0
zwx1132021 <1290443925@qq.com>
2025-06-01 22:32:31 +0800

    2025 6-1 22:32 修改文字描述

--- src/App.vue
+++ src/App.vue
@@ -66,7 +66,7 @@
 // import timeSharingFun from './components/26-timeSharingFun/index.vue' // 时间分片函数
 // import fileStreamDownload from './components/28-fileStreamDownload/index.vue' // 文件流下载
 // import beautifyJSON from './components/29-beautifyJSON/index.vue' // JSON美化
-import parallaxScrolling from './components/30-parallaxScrolling/index.vue' // 视差滚动效果
+import parallaxScrolling from './components/30-parallaxScrolling/index.vue' // 视差滚动动效
 </script>
 
 <style>
